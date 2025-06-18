67d131e5593b31dc201294e88520e5ad9bf25375
midevaa <natmalwa77@gmail.com>
2025-06-18 19:34:42 +0300

    Hello till stop

--- src/Hello/HelloTillStop.java
+++ src/Hello/HelloTillStop.java
@@ -15,4 +15,5 @@ public class HelloTillStop {
             choice = sc.next();
         }
     }}
+//
 
--- src/Hello/main.java
+++ src/Hello/main.java
@@ -1,4 +1,7 @@
 package Hello;
 
 public class main {
-}
+    public static void main (String[]args){
+        HelloTillStop stop = new HelloTillStop();
+        stop.showHello();
+    }}
