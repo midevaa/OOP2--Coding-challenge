b1491574c55553350b807d03392ebefa66ca07b9
midevaa <natmalwa77@gmail.com>
2025-07-07 16:01:34 +0300

    Updated Arithmetic

--- src/Arithmetic/ArithmeticException1.java
+++ src/Arithmetic/ArithmeticException1.java
@@ -1,6 +1,6 @@
 package Arithmetic;
 
-public class ArithmeticException {
+public class ArithmeticException1 {
     public static void main(String[] args) {
         int number1 = 300;
         int number2 = 0;
