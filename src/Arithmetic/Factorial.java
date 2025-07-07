e3476018cd8a9a650dec1d86422838e18e65a253
midevaa <natmalwa77@gmail.com>
2025-07-07 16:44:34 +0300

    Recussion Factorial

--- src/Arithmetic/Factorial.java
+++ src/Arithmetic/Factorial.java
@@ -1,4 +1,15 @@
 package Arithmetic;
 
 public class Factorial {
+    public static int factorial(int y){
+        if (y==0||y==1){
+            return 1;
+        }else{
+            return y*factorial(y-1);
+        }
+    }
+    public static void main(String[] args){
+        int result = factorial(5);
+        System.out.print("Factorial: " +result);
+    }
 }
